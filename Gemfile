55186a1ebb1bd69ba8e7e752a2d26e54c5497d05
zlahham <zlahham@gmail.com>
2015-08-13 12:01:35 +0100

    Factory girl testing working

--- Gemfile
+++ Gemfile
@@ -1,15 +1,17 @@
 # A sample Gemfile
 source "https://rubygems.org"
 
-gem 'rspec'
-gem 'capybara'
-gem 'rspec-sinatra'
 gem 'sinatra'
 gem 'bcrypt'
 gem 'sinatra-flash'
-
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
-gem 'database_cleaner'
-
 gem 'pg'
+
+group :test, :development do
+  gem 'capybara'
+  gem 'database_cleaner'
+  gem 'factory_girl'
+  gem 'rspec'
+  gem 'rspec-sinatra'
+end
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,28 +1,35 @@
 feature 'User sign up' do
 
   scenario 'I can sign up as a new user' do
-    expect { sign_up }.to change(User, :count).by(1)
+    user = create :user
+    expect { sign_up(user) }.to change(User, :count).by(1)
     expect(page).to have_content('Welcome, alice@example.com')
     expect(User.first.email).to eq('alice@example.com')
   end
 
   scenario 'with a password that does not match' do
-    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+    user = create :user, password_confirmation: 'wrong'
+    expect { sign_up(user) }.not_to change(User, :count)
     expect(current_path).to eq('/users') # current_path is a helper provided by Capybara
     expect(page).to have_content 'Password and confirmation password do not match'
   end
 
   scenario 'without an email in the field' do
-    expect { sign_up(email: '') }.not_to change(User, :count)
+    user = create :user, email: ''
+    expect { sign_up(user) }.not_to change(User, :count)
     expect(current_path).to eq('/users') # current_path is a helper provided by Capybara
     expect(page).to have_content 'You must fill in a valid email'
   end
 
-  def sign_up(email: 'alice@example.com', password: '12345678', password_confirmation: '12345678')
+  scenario 'I cannot sign up' do
+
+  end
+
+  def sign_up(user)
     visit '/users/new'
-    fill_in :email, with: email
-    fill_in :password, with: password
-    fill_in :password_confirmation, with: password_confirmation
+    fill_in :email, with: user.email
+    fill_in :password, with: user.password
+    fill_in :password_confirmation, with: user.password_confirmation
     click_button 'Sign up'
   end
 end
